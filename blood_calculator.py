78e337628f7fc3dd087a972e1fa443dc77b83b4c
Steven Parker <smp113@duke.edu>
2022-01-12 16:30:02 -0500

    Analyze HDL test result for class

--- blood_calculator.py
+++ blood_calculator.py
@@ -21,4 +21,13 @@ def accept_input(test_name):
 	entry = input("Enter the {} test Result: ".format(test_name))
 	return int(entry)
 	
+def check_HDL(HDL_value):
+	if HDL_value >= 60:
+		ans = "Normal"
+	elif 60 > HDL_value >= 40:
+		ans = "Borderline low"
+	else:
+		ans = "Low"
+	return ans
+	
 interface()
