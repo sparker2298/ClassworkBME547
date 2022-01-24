dacdcb51620313d5d56e0047a67c680d3f3c56b5
Steven Parker <smp113@duke.edu>
2022-01-24 16:17:42 -0500

    Modularity sample (not complete)

--- dosing.py
+++ dosing.py
@@ -6,34 +6,46 @@
 """
 
 
-def dose_amount():
-    print("Day One Dosing Guidelines")
-    print("")
-    print("Choose diagnosis:")
-    print("1 - Acute otitis media")
-    print("2 - Acute bacterial sinusitis")
-    print("3 - Community-acquired pneumonia")
-    print("4 - Pharyngitis/tonsilitis")
-    diagnosis = int(input("Enter a number: "))
-    print("PATIENT WEIGHT")
-    print("Enter patient weight followed by units of kg or lb.")
-    print("Examples:  65.3 lb      21.0 kg")
-    weight_input = input("Enter weight: ")
-    weight_data = weight_input.split(" ")
-    weight = float(weight_data[0])
-    units = weight_data[1]
-    if units == "lb":
-        weight = weight / 2.205
-    dosages_mg_per_kg = [30, 10, 10, 12]
-    dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
-    dosage_mg_first_day = weight * dosage_mg_per_kg
+def choose_diagnosis():
+	print("Day One Dosing Guidelines")
+	print("")
+	print("Choose diagnosis:")
+	print("1 - Acute otitis media")
+	print("2 - Acute bacterial sinusitis")
+	print("3 - Community-acquired pneumonia")
+	print("4 - Pharyngitis/tonsilitis")
+	diagnosis = int(input("Enter a number: "))
+	
+	return diagnosis
+	
+def enter_weight():
+	print("PATIENT WEIGHT")
+	print("Enter patient weight followed by units of kg or lb.")
+	print("Examples:  65.3 lb      21.0 kg")
+	weight_input = input("Enter weight: ")
+	weight_data = weight_input.split(" ")
+	weight = float(weight_data[0])
+	units = weight_data[1]
+	if units == "lb":
+		weight = weight / 2.205
+	return weight
+	
+def dose_amount(diagnosis, weight):
+	dosages_mg_per_kg = [30, 10, 10, 12]
+	dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
+	dosage_mg_first_day = weight * dosage_mg_per_kg
+	return dosage_mg_per_kg
+
+def output(weight, dosage_mg_first_day):
     print("CORRECT DOSAGE")
     print("For a patient weighing {:.1f} kg,".format(weight))
-    print("  the correct dosage is {:.1f} mg the first day"
-          .format(dosage_mg_first_day))
+    print("  the correct dosage is {:.1f} mg the first day".format(dosage_mg_first_day))
 
 
 if __name__ == '__main__':
-    dose_amount()
+	diagnosis = choose_diagnosis()
+	weight = enter_weight()
+	dosage_mg_first_day = dose_amount(diagnosis,weight)
+	output(weight,dosage_mg_first_day)
 
 
