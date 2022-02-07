bb515015c93677f41c3f06168675fad09797ce3d
Steven Parker <smp113@duke.edu>
2022-02-07 10:27:50 -0500

    Added a unit test file before making my make_line function

---
+++ test_make_line.py
@@ -0,0 +1,10 @@
+import pytest
+
+def test_get_line():
+    from make_line import get_line
+    tup1 = (2,2)
+    tup2 = (3,3)
+    new_x = 1
+    answer = get_line(tup1,tup2, new_x)
+    
+    assert answer == 1
